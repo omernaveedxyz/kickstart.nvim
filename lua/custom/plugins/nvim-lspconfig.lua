e58a04258cfdbb6492c00e00cc933f96cbc4bed8
Omer Naveed <198643919+omernaveedxyz@users.noreply.github.com>
2025-07-20 15:31:39 -0500

    Enable lsp for Typescript & Astro

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -205,8 +205,8 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        ts_ls = {},
+        astro = {},
 
         lua_ls = {
           -- cmd = { ... },
