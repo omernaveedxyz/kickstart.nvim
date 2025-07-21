cab30c6813812ea4ccf3d7a89da34bb1c890ae1d
Omer Naveed <198643919+omernaveedxyz@users.noreply.github.com>
2025-07-20 23:13:54 -0500

    Refactor oil.nvim configuration

--- lua/custom/plugins/oil.nvim.lua
+++ lua/custom/plugins/oil.nvim.lua
@@ -1,61 +1,62 @@
 return {
   {
     'stevearc/oil.nvim',
+    opts = {
+      -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+      -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+      default_file_explorer = true,
+      -- Id is automatically added at the beginning, and name at the end
+      -- See :help oil-columns
+      columns = {
+        'icon',
+        'permissions',
+        'size',
+        'mtime',
+      },
+      -- Window-local options to use for oil buffers
+      win_options = {
+        wrap = false,
+        signcolumn = 'yes',
+        cursorcolumn = false,
+        foldcolumn = '0',
+        spell = false,
+        list = false,
+        conceallevel = 3,
+        concealcursor = 'nvic',
+      },
+      -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+      delete_to_trash = true,
+      -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+      -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+      -- Additionally, if it is a string that matches "actions.<name>",
+      -- it will use the mapping at require("oil.actions").<name>
+      -- Set to `false` to remove a keymap
+      -- See :help oil-actions for a list of all available actions
+      keymaps = {
+        ['g?'] = { 'actions.show_help', mode = 'n' },
+        ['<CR>'] = 'actions.select',
+        ['<C-s>'] = { 'actions.select', opts = { vertical = true } },
+        ['<C-h>'] = { 'actions.select', opts = { horizontal = true } },
+        ['<C-t>'] = { 'actions.select', opts = { tab = true } },
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = { 'actions.close', mode = 'n' },
+        ['<C-l>'] = 'actions.refresh',
+        ['-'] = { 'actions.parent', mode = 'n' },
+        ['_'] = { 'actions.open_cwd', mode = 'n' },
+        ['`'] = { 'actions.cd', mode = 'n' },
+        ['~'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
+        ['gs'] = { 'actions.change_sort', mode = 'n' },
+        ['gx'] = 'actions.open_external',
+        ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
+        ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
+      },
+    },
     config = function()
-      require('oil').setup {
-        -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
-        -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
-        default_file_explorer = true,
-        -- Id is automatically added at the beginning, and name at the end
-        -- See :help oil-columns
-        columns = {
-          'icon',
-          'permissions',
-          'size',
-          'mtime',
-        },
-        -- Window-local options to use for oil buffers
-        win_options = {
-          wrap = false,
-          signcolumn = 'yes',
-          cursorcolumn = false,
-          foldcolumn = '0',
-          spell = false,
-          list = false,
-          conceallevel = 3,
-          concealcursor = 'nvic',
-        },
-        -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
-        delete_to_trash = true,
-        -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
-        -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
-        -- Additionally, if it is a string that matches "actions.<name>",
-        -- it will use the mapping at require("oil.actions").<name>
-        -- Set to `false` to remove a keymap
-        -- See :help oil-actions for a list of all available actions
-        keymaps = {
-          ['g?'] = { 'actions.show_help', mode = 'n' },
-          ['<CR>'] = 'actions.select',
-          ['<C-s>'] = { 'actions.select', opts = { vertical = true } },
-          ['<C-h>'] = { 'actions.select', opts = { horizontal = true } },
-          ['<C-t>'] = { 'actions.select', opts = { tab = true } },
-          ['<C-p>'] = 'actions.preview',
-          ['<C-c>'] = { 'actions.close', mode = 'n' },
-          ['<C-l>'] = 'actions.refresh',
-          ['-'] = { 'actions.parent', mode = 'n' },
-          ['_'] = { 'actions.open_cwd', mode = 'n' },
-          ['`'] = { 'actions.cd', mode = 'n' },
-          ['~'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
-          ['gs'] = { 'actions.change_sort', mode = 'n' },
-          ['gx'] = 'actions.open_external',
-          ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
-          ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
-        },
+      require('oil').setup()
 
-        -- Open the directory explorer in the current working directory
-        -- See `:help Oil`
-        vim.keymap.set('n', '<leader>pv', '<CMD>Oil --float<CR>', { desc = '[P]roject [V]iew' }),
-      }
+      -- Open the directory explorer in the current working directory
+      -- See `:help Oil`
+      vim.keymap.set('n', '<leader>pv', '<CMD>Oil --float<CR>', { desc = '[P]roject [V]iew' })
     end,
     -- Optional dependencies
     dependencies = { { 'echasnovski/mini.icons', opts = {} } },
